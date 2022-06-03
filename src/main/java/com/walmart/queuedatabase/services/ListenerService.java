1fcb6cbd0a6ba890b67f9432d16b7decd10ec880
Thymmz <atharva.deshpande228@gmail.com>
2022-06-03 13:23:02 -0500

    comment and cleanup

--- src/main/java/com/walmart/queuedatabase/services/ListenerService.java
+++ src/main/java/com/walmart/queuedatabase/services/ListenerService.java
@@ -2,20 +2,15 @@ package com.walmart.queuedatabase.services;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ibm.jms.JMSObjectMessage;
 import com.walmart.queuedatabase.model.People;
-import com.walmart.queuedatabase.model.PeopleModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
-import javax.jms.ObjectMessage;
 import javax.jms.TextMessage;
-import java.io.*;
 
 @Service
 public class ListenerService {
@@ -28,29 +23,13 @@ public class ListenerService {
 
     @JmsListener(destination = "INPUT")
     public void listen(Message message) throws JMSException, JsonProcessingException {
+        //Get text message from queue and map it to JPA Entity
         TextMessage textMessage = (TextMessage) message;
         String personString = textMessage.getText();
         ObjectMapper mapper = new ObjectMapper();
         People person = mapper.readValue(personString, People.class);
+
+        //Save the JPA message entity to database
         peopleService.savePersontoDb(person);
-    }
-
-    public Object cloneObject(Object originalObject) {
-        Object clonedObject = null;
-        try {
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-            objectOutputStream.writeObject(originalObject);
-            //retrieve back
-            ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
-            ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
-            clonedObject = objectInputStream.readObject();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        } catch (ClassNotFoundException cnfe) {
-            cnfe.printStackTrace();
-        }
-
-        return clonedObject;
     }
 }
